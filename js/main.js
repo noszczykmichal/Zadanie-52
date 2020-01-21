440fb800bffbe615fea8657883ee6fa8b9610f86
Michał Noszczyk <noszczykmichal@wp.pl>
2020-01-22 00:00:37 +0100

    weryfikacja poprawnego dzialania funkcji

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function takeData(){
+    let firstname=window.prompt('Podaj imię: ');
+    let lastname=window.prompt('Podaj nazwisko: ');
+    let email=window.prompt('Podaj adres E-mail: ');
+
+    console.log(firstname);
+ }
+
+takeData();
